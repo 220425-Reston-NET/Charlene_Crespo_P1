23e51b2b992b914c82315a33ac7fe70fba8b5be6
Charlene-Crespo-FW <Charleneicrespo@icloud.com>
2022-06-06 11:03:10 -0400

    second update

--- RepairShopv2Test/RepairShopv2ModelTest.cs
+++ RepairShopv2Test/RepairShopv2ModelTest.cs
@@ -1,24 +1,24 @@
-using RepairShopv2Model;
-using Xunit;
+// using RepairShopv2Model;
+// using Xunit;
 
-namespace RepairShopv2Test
-{
-    public class RepairShopv2ModelTest
-    {
-        [Fact]
-        public void CustomerNumber_Should_Set_ValidData()
-        {
-            //Arrange
-            Customers custObj = new Customers();
-            long Number = 1234567890;
+// namespace RepairShopv2Test
+// {
+//     public class RepairShopv2ModelTest
+//     {
+//         [Fact]
+//         public void CustomerNumber_Should_Set_Number()
+//         {
+//             //Arrange
+//             Customers custObj = new Customers();
+//             long Number = 1234567890;
 
-            //Act
-            custObj.Number = Number;
+//             //Act
+//             custObj.Number = Number;
 
-            //Assert
-            Assert.NotNull(custObj.Number);
-            Assert.Equal(Number, custObj.Number);
+//             //Assert
+//             Assert.NotNull(custObj.Number);
+//             Assert.Equal(Number, custObj.Number);
 
-        }
-    }
-}
+//         }
+//     }
+// }
